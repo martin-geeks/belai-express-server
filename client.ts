dc6c8b9738943dcf3e1d8e42b07081122fdee476
martin-geeks <martin.mt.geek@gmail.com>
2022-08-08 08:20:27 +0200

    test

--- client.ts
+++ client.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-async function  main(){
+/*async function  main(){
   return new Promise((resolve,reject) =>{
   mongoose.connect('mongodb://localhost:27017/belai-express').then(()=>{
   
@@ -12,6 +12,18 @@ async function  main(){
   
 });
   });
+}*/
+async function  main(){
+  return new Promise((resolve,reject) =>{
+  mongoose.connect('mongodb+srv://martintembo1:$9th_April_2017&@cluster0.x6koa.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+  
+    
+    resolve(mongoose);
+}).catch((err:any) => {
+  console.log(err);
+  reject(err)
+  
+});
+  });
 }
-
 export {main}
